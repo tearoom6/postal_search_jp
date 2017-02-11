46d6b958bc436d5030b619a2aafc655e2192a782
Tomohiro Murota <tomohiro.murota@gmail.com>
2017-02-12 04:06:51 +0900

    Add RSpec codes

--- spec/postal_search_jp_spec.rb
+++ spec/postal_search_jp_spec.rb
@@ -17,4 +17,20 @@ RSpec.describe PostalSearchJp do
     expect(PostalSearchJp.configs[:s3_path]).to eq('my_postal_search_jp')
   end
 
+  it 'searches by address' do
+    records = PostalSearchJp.search_by_address('井の頭')
+    expect(records.size).to eq(1)
+    expect(records.first.postal_code).to eq('1810001')
+    records = PostalSearchJp.search_by_address('東京')
+    expect(records.size).to eq(3813)
+    records = PostalSearchJp.search_by_address('1810001')
+    expect(records).to be_empty
+  end
+
+  it 'finds by postal code' do
+    expect(PostalSearchJp.find_by_postal_code('1810001').prefecture).to eq('東京都')
+    expect(PostalSearchJp.find_by_postal_code('1810001').city).to eq('三鷹市')
+    expect(PostalSearchJp.find_by_postal_code('181-0001')).to be_nil
+  end
+
 end
